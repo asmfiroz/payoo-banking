60300c7ec84e90a21f7b6167265ac63266ef2165
asmfiroz <asmfiroz76935@gmail.com>
2026-07-09 02:15:35 +0600

    Modify the Cashout page and design the Add Money Form

--- script/cashout.js
+++ script/cashout.js
@@ -8,13 +8,11 @@ document.getElementById("cashout-btn").addEventListener("click", function(){
     }
     //2--get the amount
     const cashoutAmount= getValueFromInput("cashout-amount");
-    // 3-- get the current Balance
-    const balanceElement= document.getElementById("balance");
-    const balance= balanceElement.innerText;
-    console.log(balance);
+    
+    const currentBalance= getBalance();
 
     //4-- calculate balance
-    const newBalance= Number(balance)- Number(cashoutAmount);
+    const newBalance= currentBalance- Number(cashoutAmount);
     console.log(newBalance);
     if(newBalance<0){
         alert("Invalid Amount");
@@ -23,8 +21,7 @@ document.getElementById("cashout-btn").addEventListener("click", function(){
     const pin= getValueFromInput("cashout-pin");
     if(pin=="1234"){
         alert("Cashout Successfull");
-        balanceElement.innerText= newBalance;
-
+        setBalance(newBalance);
     }
     else{
         alert("Invalid pin");
